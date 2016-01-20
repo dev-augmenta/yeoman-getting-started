375c0d2860831b0b75fd5c32bc947c7b8cca790a
Erik Podetti <erik.podetti@gmail.com>
2016-01-20 18:29:55 +0100

    Added basic form interaction

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,8 +10,17 @@
 angular.module('yeomanGettingStartedApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = [
-      'Item 1',
-      'Item 2',
-      'Item 3'
+		'Item 1',
+		'Item 2',
+		'Item 3'
+
     ];
+	$scope.addTodo = function(){
+		$scope.todos.push($scope.todo);
+		$scope.todo = '';
+	};
+
+	$scope.removeTodo = function(index){
+		$scope.todos.splice(index, 1);
+	};
   });
